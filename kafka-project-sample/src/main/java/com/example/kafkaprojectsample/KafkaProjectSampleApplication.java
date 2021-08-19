0a187144344d4145eb846c7a24aed5a3b6a15ae5
hsyoon900528 <szpdkss3@gmail.com>
2021-08-19 10:15:36 +0900

    20210819commit2

---
+++ kafka-project-sample/src/main/java/com/example/kafkaprojectsample/KafkaProjectSampleApplication.java
@@ -0,0 +1,30 @@
+package com.example.kafkaprojectsample;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaProjectSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProjectSampleApplication.class, args);
+    }
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("topic1").partitions(1).replicas(1).build();
+
+    }
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template){
+        return args -> {
+            template.send("topic1","test");
+        };
+    }
+
+}
